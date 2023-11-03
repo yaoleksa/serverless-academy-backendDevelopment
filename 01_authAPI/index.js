709b62bcf54b0cf6655cbf08e9fc06b258f3a388
yaoleksa <mryaremchyk@gmail.com>
2023-11-03 18:08:15 +0200

    improve

--- 01_authAPI/index.js
+++ 01_authAPI/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const pg = require('pg');
 const bcrypt = require('bcrypt');
+const { createClient } = require('@supabase/supabase-js');
 //define port
 const port = process.env.PORT || 3000;
 // define application itself
@@ -9,24 +10,52 @@ const app = express();
 // define middleware
 app.use(express.json({extended: false}));
 app.use((req, res, next) => {
+    if(req.method == 'GET') {
+        next();
+        return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Accept', 'application/json');
     const data = [];
     req.on('data', chunk => {
         data.push(chunk);
     });
     req.on('end', () => {
-        req.body = JSON.parse(Buffer.concat(data).toString());
+        if(data.length == 0) {
+            res.send('Body is empty, there are no required parameters\n');
+            next();
+            return;
+        }
+        const body = JSON.parse(Buffer.concat(data).toString());
+        if(!body.email) {
+            res.send('Email is missing. Email is required\n');
+            next();
+            return;
+        }
+        if(body.email && !/.{1,}@.{1,}\.{1,}/.test(body.email)) {
+            res.send('Invalid email format\n');
+            next();
+            return;
+        }
+        if(!body.password) {
+            res.send('Password is missing. Password is required\n');
+            next();
+            return;
+        }
+        req.body = body;
     });
     next();
-})
+});
 // define method handlers
 app.get('/', (req, res) => {
     res.send(`successful request at: ${new Date()}`);
 });
-app.post('/auth/sign-in', (req, res) => {
+app.post('/auth/sign-in', (req, res, next) => {
     req.on('end', () => {
-        console.log(`body now: ${req.body.email}`);
-    })
-    res.sendStatus(200);
+        if(req.body && req.body.email && req.body.password) {
+            res.sendStatus(200);
+        }
+    });
 });
 app.post('/auth/sign-up', (req, res) => {
     res.sendStatus(201);
