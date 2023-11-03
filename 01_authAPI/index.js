ee7838e05e3f82ef682eb4394db6e5d22d24a080
yaoleksa <mryaremchyk@gmail.com>
2023-11-03 13:24:03 +0200

    built basic structure

--- 01_authAPI/index.js
+++ 01_authAPI/index.js
@@ -6,9 +6,23 @@ const bcrypt = require('bcrypt');
 const port = process.env.PORT || 3000;
 // define application itself
 const app = express();
+// define middleware
+app.use((req, res, next) => {
+    // WARNING!!! It's not a real middleware
+    console.log('This is just am example. Real middleware will be added letter');
+    next();
+});
+// define method handlers
 app.get('/', (req, res) => {
     res.send(`successful request at: ${new Date()}`);
 });
+app.post('/auth/sign-in', (req, res) => {
+    res.sendStatus(200);
+});
+app.post('/auth/sign-up', (req, res) => {
+    res.sendStatus(201);
+});
+// start listening to the application on the defined port
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
