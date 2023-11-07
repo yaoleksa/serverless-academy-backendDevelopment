95662219ce18419f4c6e8d096acbd2412e4fa647
yaoleksa <mryaremchyk@gmail.com>
2023-11-07 22:38:34 +0200

    Connected with db

--- 03_json_storage/index.js
+++ 03_json_storage/index.js
@@ -1,12 +1,26 @@
 // define all required packages
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
+const { Client } = require('pg');
+const { response } = require('express');
+// define database client
+const client = new Client({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    port: process.env.DBPORT,
+    password: process.env.PASSWORD
+});
+// Connect with database
+client.connect();
 // define supabase client
 const supabase = createClient(process.env.URL, process.env.KEY);
 // define application itself
 const app = express();
-app.get('/', (req, res) => {
-    res.send(`ip: ${req.socket.remoteAddress}`);
+app.get('/json', (req, res) => {
+    client.query('SELECT * FROM "jsonbase";').then(response => {
+        res.send(JSON.stringify(response.rows, null, 2));
+    });
 });
 app.listen({
     port: process.env.PORT,
