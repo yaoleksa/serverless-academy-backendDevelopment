2d4f0b1fc405ce75e4ceacd412bc1436ad8e5942
yaoleksa <mryaremchyk@gmail.com>
2023-11-14 18:14:55 +0200

    improve third one

--- 03_json_storage/index.js
+++ 03_json_storage/index.js
@@ -1,92 +1,243 @@
 // define all required packages
 const express = require('express');
-const { Client } = require('pg');
 const fs = require('fs');
-// define database client
-const client = new Client({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    port: process.env.DBPORT,
-    password: process.env.PASSWORD
-});
-// Connect with database
-client.connect();
+const { createClient } = require('@supabase/supabase-js');
+const { parse } = require('csv-parse');
+//define port
+const port = process.env.PORT || 3000;
+// define DB url
+const superbaseUrl = process.env.URL;
+// define API key
+const superbaseKey = process.env.KEY;
+// define superbase client
+const superbase = createClient(superbaseUrl, superbaseKey);
+// variables to store tokens
+let refreshToken;
+let token;
 // define application itself
 const app = express();
 // define middleware
 app.use((req, res, next) => {
-    if(req.method == 'GET') {
-        next();
-        return;
-    }
-    if(Object.keys(req.query).length > 0) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Accept', 'application/json');
+    if(req.method == 'GET' || req.query.file) {
         next();
         return;
     }
     const data = [];
     req.on('data', chunk => {
-        if(chunk) {
-            data.push(chunk);
-        }
+        data.push(chunk);
     });
     req.on('end', () => {
-        try {
-            if(data.length == 0) {
-                next();
-                return;
-            }
-            req.body = JSON.parse(Buffer.concat(data).toString());
-        } catch(exception) {
-            res.send(exception.message);
+        if(data.length == 0 && req.method != 'PUT') {
+            res.send('Body is empty, there are no required parameters\n');
             next();
             return;
         }
+        const body = JSON.parse(Buffer.concat(data).toString());
+        if(!body.email && req.method != 'PUT') {
+            res.send('Email is missing. Email is required\n');
+            next();
+            return;
+        }
+        // validate email
+        if(body.email && !/.{1,}@.{1,}\.{1,}/.test(body.email) && req.method != 'PUT') {
+            res.send('Invalid email format\n');
+            next();
+            return;
+        }
+        if(!body.password && req.method != 'PUT') {
+            res.send('Password is missing. Password is required\n');
+            next();
+            return;
+        }
+        req.body = body;
     });
     next();
 });
-// define HTTP methods' handlers
-app.get('/:name', (req, res, next) => {
-    client.query('SELECT * FROM "jsonbase" WHERE name=$1', [req.params.name]).then(response => {
-        if(response.rows.length == 0) {
-            res.send('Record with such name does not exit\n');
-            next();
-            return;
-        } else {
-            res.send(`${JSON.stringify(response.rows, null, 2)}\n`);
-            next();
-            return;
+// define IP to number converter function
+const IPtoNum = (ip) => {
+    const dot = ip.split('.');
+    if(dot.length != 4) {
+        return NaN;
+    }
+    try {
+        return 256**3 * parseInt(dot[0]) + 256**2 * parseInt(dot[1]) + 256 * parseInt(dot[2]) + parseInt(dot[3]);
+    } catch(exception) {
+        console.error(exception.message);
+    }
+}
+// variable to store IPs' range
+const countries = [];
+// read file with IPs' range
+// fs.createReadStream('IP2LOCATION-LITE-DB1.CSV').pipe(parse({delimiter: ',', from_line: 1})).on('data', row => {
+//     countries.push(row);
+// }).on('end', () => {
+//     console.log('finish read file');
+// });
+// define method handlers
+app.get('/', (req, res) => {
+    const clientIP = req.header('x-forwarded-for') || req.socket.remoteAddress;
+    const IPasNum = IPtoNum(clientIP);
+    const result = countries.filter(el => el[0] <= IPasNum && IPasNum <= el[1]);
+    if(result.length != 1) {
+        console.log(result);
+    }
+    res.send(`${result[0][3]} - ${clientIP}\n`);
+});
+app.get('/me', (req, res, next) => {
+    if(!req.headers.authorization) {
+        res.send('Request header does not contain authorization key. JWT token is required\n');
+        next();
+        return;
+    }
+    const jwt = req.headers.authorization.split(' ')[1];
+    if(!jwt) {
+        res.send("Unproper Authorization format. You should use Bearer your_token template\n");
+        next();
+        return;
+    }
+    try {
+        superbase.auth.getUser(jwt).then(response => {
+            if(!response.data.user) {
+                res.send("Invalid JWT token\n");
+                next();
+                return;
+            }
+            res.send(JSON.stringify({
+                "success": true,
+                "data": {
+                    "id": response.data.user.id,
+                    "email": response.data.user.email
+                }
+            }, null, 2));
+        });
+    } catch(exception) {
+        res.send(JSON.stringify({
+            "success": false,
+            "data": null
+        }, null, 2));
+        next();
+        return;
+    }
+});
+app.get('/:table/:name', (req, res, next) => {
+    superbase.from(req.params.table).select().eq('name', req.params.name).then(response => {
+        res.send(JSON.stringify(response.data, null, 2) + '\n');
+    });
+});
+app.post('/auth/sign-in', (req, res, next) => {
+    req.on('end', () => {
+        if(req.body && req.body.email && /.{1,}@.{1,}\.{1,}/.test(req.body.email) && req.body.password) {
+            try {
+                superbase.auth.signInWithPassword({
+                    email: req.body.email,
+                    password: req.body.password
+                }).then(response => {
+                    if(!response.data.user) {
+                        if(response.error) {
+                            res.send(response.error.message + '\n');
+                            next();
+                            return;
+                        }
+                        res.send('Invalid credential\n');
+                        next();
+                        return;
+                    }
+                    token = response.data.session.access_token;
+                    refreshToken = response.data.session.refresh_token;
+                    res.send(JSON.stringify({
+                        "success": true,
+                        "data": {
+                            "id": response.data.user.id,
+                            "accessToken": token,
+                            "refreshToken": refreshToken
+                        }
+                    }, null, 2) + '\n');
+                });
+            } catch(exception) {
+                res.send(JSON.stringify({
+                    "success": false,
+                    "data": null
+                }, null, 2));
+            }
         }
     });
 });
-app.put('/:name', (req, res, next) => {
+app.post('/auth/sign-up', (req, res, next) => {
+    req.on('end', () => {
+        if(req.body && req.body.email && /.{1,}@.{1,}\.{1,}/.test(req.body.email) && req.body.password) {
+            try {
+                if(req.body.password.length < 6) {
+                    res.send("Password should have at least 6 signs\n");
+                    next();
+                    return;
+                }
+                superbase.auth.signUp({
+                    email: req.body.email,
+                    password: req.body.password
+                }).then(response => {
+                    if(!response.data.user) {
+                        console.log(response);
+                        if(response.error) {
+                            res.send(response.error.message + '\n');
+                            next();
+                            return;
+                        }
+                        res.send("Unable to create a new usser\n");
+                        next();
+                        return;
+                    }
+                    token = response.data.session.access_token,
+                    refreshToken = response.data.session.refresh_token;
+                    res.send(JSON.stringify({
+                        "success": true,
+                        "data": {
+                            "id": response.data.user.id,
+                            "accessToken": token,
+                            "refreshToken": refreshToken
+                        }
+                    }, null, 2));
+                });
+            } catch(exception) {
+                res.send(JSON.stringify({
+                    "success": false,
+                    "data": null
+                }, null, 2));
+                next();
+                return;
+            }
+        }
+    });
+});
+app.put('/:table/:name', (req, res, next) => {
     if(req.query.file) {
         const content = fs.readFileSync(req.query.file);
-            client.query('INSERT INTO jsonbase(name, data) VALUES($1, $2) RETURNING *;', [req.params.name, content]).then(response => {
-                res.send(`${JSON.stringify(response.rows, null, 2)}\n`);
-                next();
-                return;
-            }).catch(exception => {
-                res.send(exception.message);
-                next();
-                return;
-            });
+        superbase.from(req.params.table).insert({
+            name: req.params.name,
+            data: content
+        }).then(response => {
+            res.send(JSON.stringify(response.status, null, 2) + '\n');
+            next();
+            return;
+        });
     }
     req.on('end', () => {
-        if(Object.keys(req.query).length == 0) {
-            client.query('INSERT INTO jsonbase(name, data) VALUES($1, $2) RETURNING *;', [req.params.name, req.body]).then(response => {
-                res.send(`${JSON.stringify(response.rows, null, 2)}\n`);
-            }).catch(exception => {
-                res.send(exception.message);
-                next();
-                return;
+        if(req.body && Object.keys(req.body).length > 0) {
+            superbase.from(req.params.table).insert({
+                name: req.params.name,
+                data: req.body
+            }).then(response => {
+                res.send(JSON.stringify(response, null, 2) + '\n');
             });
         }
     });
 });
+// start listening to the application on the defined port
 app.listen({
-    port: process.env.PORT,
+    port: port,
     host: '0.0.0.0'
 }, () => {
-    console.log(`http://localhost:${process.env.PORT}`);
-})
+    console.log(`http://localhost:${port}`);
+});
